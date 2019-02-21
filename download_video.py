ac85172a15bfb32c941e844c6ea06204690f7121
lzc <1226740471@qq.com>
2019-02-21 23:04:26 +0800

    添加注释说明

--- download_video.py
+++ download_video.py
@@ -9,16 +9,15 @@ totle = int(r.headers['content-length']) #下载的总内容大小
 csize = 0
 percent = 0
 with open('./videos/test.mp4', "wb") as mp4:
-    for chunk in r.iter_content(chunk_size=1024):
+    for chunk in r.iter_content(chunk_size=1024): #每次获取视频字节大小
     	
         if chunk:
             mp4.write(chunk)
         csize +=1024
-        percent = round(csize / totle*100,2)
+        percent = round(csize / totle*100,2) #计算百分比，保留两位小数
 
         if csize==totle:
         	print("下载进度：%s" %(str(100)+"%"),end="")
         else :
-        	print("下载进度：%s" %(str(percent)+"%"),end='\r')
+        	print("下载进度：%s" %(str(percent)+"%"),end='\r')  #不换行实时刷新显示
 
-print("下载结束")
